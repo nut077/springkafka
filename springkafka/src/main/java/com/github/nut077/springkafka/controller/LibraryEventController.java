450bf8413dc885cbf7930169b2a3fc88ba8405e8
nut077 <nuttapon.tavee@gmail.com>
2022-12-05 19:23:03 +0700

    set send kafka

--- springkafka/src/main/java/com/github/nut077/springkafka/controller/LibraryEventController.java
+++ springkafka/src/main/java/com/github/nut077/springkafka/controller/LibraryEventController.java
@@ -1,16 +1,23 @@
 package com.github.nut077.springkafka.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.github.nut077.springkafka.entity.LibraryEvent;
+import com.github.nut077.springkafka.producer.LibraryEventProducer;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("v1")
+@RequiredArgsConstructor
 public class LibraryEventController {
 
+  private final LibraryEventProducer libraryEventProducer;
+
   @PostMapping("/library-event")
-  public ResponseEntity<LibraryEvent> createEvent(@RequestBody LibraryEvent req) {
+  public ResponseEntity<LibraryEvent> createEvent(@RequestBody LibraryEvent req) throws JsonProcessingException {
+    libraryEventProducer.sendLibraryEvent(req);
     return ResponseEntity.status(HttpStatus.CREATED).body(req);
   }
 }
